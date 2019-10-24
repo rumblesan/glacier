4968fdbe3065b17434ba610b6a100827868d6bba
Guy John <guy@rumblesan.com>
2019-10-24 10:55:41 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,22 @@
+CC         = clang
+CFLAGS     = -c -Wall
+OBJDIR     = .tmp
+SOURCES    = $(wildcard src/*.c)
+OBJECTS    = $(addprefix $(OBJDIR)/, $(SOURCES:.c=.o))
+HEADERS    = include/
+EXECUTABLE = glacier
+
+LIBS       = -lportaudio
+
+.PHONE: clean
+
+all: $(EXECUTABLE)
+
+$(EXECUTABLE): $(OBJECTS)
+	$(CC) $(LIBS) $(OBJECTS) -o $@
+
+$(OBJDIR)/%.o: %.c
+	$(CC) $(CFLAGS) -I$(HEADERS) $(LDFLAGS) $< -o $@
+
+clean:
+	rm -rf $(OBJDIR)/src/*.o $(EXECUTABLE)
---
+++ src/main.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <math.h>
+#include "portaudio.h"
+
+#include "dbg.h"
+
+/*
+** Note that many of the older ISA sound cards on PCs do NOT support
+** full duplex audio (simultaneous record and playback).
+** And some only support full duplex at lower sample rates.
+*/
+#define SAMPLE_RATE         (44100)
+#define PA_SAMPLE_TYPE      paFloat32
+#define FRAMES_PER_BUFFER   (64)
+
+typedef float SAMPLE;
+
+static int gNumNoInputs = 0;
+/* This routine will be called by the PortAudio engine when audio is needed.
+** It may be called at interrupt level on some machines so don't do anything
+** that could mess up the system like calling malloc() or free().
+*/
+
+static int passthroughCB( const void *inputBuffer, void *outputBuffer,
+                          unsigned long framesPerBuffer,
+                          const PaStreamCallbackTimeInfo* timeInfo,
+                          PaStreamCallbackFlags statusFlags,
+                          void *userData ) {
+    SAMPLE *out = (SAMPLE*)outputBuffer;
+    const SAMPLE *in = (const SAMPLE*)inputBuffer;
+    unsigned int i;
+    (void) timeInfo; /* Prevent unused variable warnings. */
+    (void) statusFlags;
+    (void) userData;
+
+    if( inputBuffer == NULL ) {
+        for( i=0; i<framesPerBuffer; i++ ) {
+            *out++ = 0;  /* left - silent */
+            *out++ = 0;  /* right - silent */
+        }
+        gNumNoInputs += 1;
+    } else {
+        for( i=0; i<framesPerBuffer; i++ ) {
+            *out++ = *in++;  /* left - clean */
+            *out++ = *in++;  /* right - clean */
+        }
+    }
+    
+    return paContinue;
+}
+
+
+/*******************************************************************/
+int main(void) {
+    PaStreamParameters inputParameters, outputParameters;
+    PaStream *stream;
+    PaError err;
+
+    err = Pa_Initialize();
+    check(err == paNoError, "could not initialize port audio");
+
+    inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
+    check (inputParameters.device != paNoDevice, "Error: No default input device.");
+    inputParameters.channelCount = 2;       /* stereo input */
+    inputParameters.sampleFormat = PA_SAMPLE_TYPE;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
+    inputParameters.hostApiSpecificStreamInfo = NULL;
+
+    outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+    check (outputParameters.device != paNoDevice, "Error: No default output device.")
+
+    outputParameters.channelCount = 2;       /* stereo output */
+    outputParameters.sampleFormat = PA_SAMPLE_TYPE;
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+    outputParameters.hostApiSpecificStreamInfo = NULL;
+
+    err = Pa_OpenStream(
+              &stream,
+              &inputParameters,
+              &outputParameters,
+              SAMPLE_RATE,
+              FRAMES_PER_BUFFER,
+              0, /* paClipOff, */  /* we won't output out of range samples so don't bother clipping them */
+              passthroughCB,
+              NULL );
+    check(err == paNoError, "could not open stream");
+
+    err = Pa_StartStream( stream );
+    check(err == paNoError, "could not start stream");
+
+    printf("Hit ENTER to stop program.\n");
+    getchar();
+    err = Pa_CloseStream( stream );
+    check(err == paNoError, "could not close stream");
+
+    printf("Finished. gNumNoInputs = %d\n", gNumNoInputs );
+    Pa_Terminate();
+    return 0;
+
+error:
+    Pa_Terminate();
+    fprintf( stderr, "An error occured while using the portaudio stream\n" );
+    fprintf( stderr, "Error number: %d\n", err );
+    fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
+    return -1;
+}
