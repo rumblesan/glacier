89bb0cee39a6fe2f4d8fef048a955cf42bc59ae1
Guy John <guy@rumblesan.com>
2019-10-27 16:51:51 +0000

    Adding beginnings of tests

---
+++ src/tests/src/test-control_message.c
@@ -0,0 +1,27 @@
+#include "minunit.h"
+
+#include "control_message.h"
+
+char *test_control_message_create() {
+
+  int buffer_number = 1;
+  enum buffer_command cmd = StartRecording;
+
+  ControlMessage *cm = cm_create(buffer_number, cmd);
+  mu_assert(cm != NULL "Could not create Control Message");
+  mu_assert(cm->buffer_number == buffer_number, "Incorrect buffer number");
+  mu_assert(cm->cmd == cmd, "Incorrect command");
+
+  cm_destroy(cm);
+  return NULL;
+}
+
+char *all_tests() {
+  mu_suite_start();
+
+  mu_run_test(test_control_message_create);
+
+  return NULL;
+}
+
+RUN_TESTS(all_tests);
