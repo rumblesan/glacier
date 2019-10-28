4f66708000dfda44e55d98ef3ecfc2600603a5e2
Guy John <guy@rumblesan.com>
2019-10-28 12:39:56 +0000

    tweaks to unit test setup

--- src/tests/test_all.c
+++ src/tests/test_all.c
@@ -5,26 +5,31 @@
 
 int tests_run;
 
+typedef char* (test_suite_func)();
+int run_suite(test_suite_func suite, char *suite_name) {
+  printf("%s tests\n", suite_name);
+  char *result = (*suite)();
+  if (result != 0) {
+    printf("%s\n", result);
+    return 1;
+  } else {
+    printf("suite passed\n\n");
+    return 0;
+  }
+}
+
 int main(int argc, char *argv[]) {
 
-    printf("\n");
-    printf("*****************\n");
-    printf("* Running tests *\n");
-    printf("*****************\n\n");
-
-    int result = 0;
+    printf("\n*****************");
+    printf("\n* Running tests *");
+    printf("\n*****************\n\n");
 
     tests_run = 0;
-    char *control_message_results = test_control_message();
-    if (control_message_results != 0) {
-        printf("%s\n", control_message_results);
-    } else {
-        printf("GOL TESTS PASSED\n");
-    }
-    printf("    Tests run: %d\n", tests_run);
-    printf("\n");
 
-    result = result || (control_message_results != 0);
+    int result = 0;
+    result = result || run_suite(&test_control_message, "Control Message");
+
+    printf("Tests run: %d\n\n", tests_run);
 
     return result;
 }
